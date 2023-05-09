9cb5be4bef2e312c8a032d29ea759d0c08a1d92c
Sumanjit38 <sumanjit38@gmail.com>
2023-05-10 04:08:31 +0530

    update contract

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Dappcord is ERC721 {
+    uint256 public totalSupply;
     uint256 public totalChannels = 0;
     address public owner;
 
@@ -15,16 +16,47 @@ contract Dappcord is ERC721 {
     }
 
     mapping(uint256 => Channel) public channels;
+    mapping(uint256 => mapping(address => bool)) public hasJoined;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     constructor(string memory _name, string memory _symbol) 
         ERC721(_name, _symbol){
             owner = msg.sender;
     }
 
-    function createChannel(string memory _name, uint256 _cost) public {
+    function createChannel(string memory _name, uint256 _cost) public onlyOwner {
  
         // Create a Channel
+        //require(msg.sender == owner);
         totalChannels++;
         channels[totalChannels] = Channel(totalChannels, _name, _cost);
+    }
+
+    function mint(uint256 _id) public payable {
+        require(_id != 0);
+        require(_id <= totalChannels);
+        require(hasJoined[_id][msg.sender] == false);
+        require(msg.value >= channels[_id].cost);
+        // Join channel
+        hasJoined[_id][msg.sender] = true;
+
+        //Mint NFT
+        totalSupply ++;
+        _safeMint(msg.sender, totalSupply);
+    }
+
+    function getChannel(uint256 _id) public view returns (Channel memory) {
+ 
+        // Get a Channel
+        return channels[_id];
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
     }
 }
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -21,6 +21,10 @@ describe("Dappcord", function () {
     const Dappcord = await ethers.getContractFactory("Dappcord")
     dappcord = await Dappcord.deploy(NAME, SYMBOL)
 
+    // Create a channel
+    const transaction = await dappcord.connect(deployer).createChannel("general", tokens(1))
+    await transaction.wait()
+
   })
 
   describe("Deployment", function() {
@@ -52,6 +56,70 @@ describe("Dappcord", function () {
 
   describe("Creating Channels", () => {
 
+    it("Returns total channels", async () => {
+      const result = await dappcord.totalChannels()
+      expect(result).to.be.equal(1)
+    })
+
+    it("Returns channel attributes", async () => {
+      const channel = await dappcord.getChannel(1)
+      expect(channel.id).to.be.equal(1)
+      expect(channel.name).to.be.equal("general")
+      expect(channel.cost).to.be.equal(tokens(1))
+    })
+
+
+  })
+
+  describe("Joining Channels", () => {
+    const ID =1
+    const AMOUNT = ethers.utils.parseUnits("1", 'ether')
+
+    beforeEach(async () => {
+      const transaction = await dappcord.connect(user).mint(ID, { value: AMOUNT })
+      await transaction.wait()
+    })
+
+    it("Joins the user", async () => {
+      const result = await dappcord.hasJoined(ID, user.address)
+      expect(result).to.be.equal(true)
+    })
+
+    it('Increases total supply', async () => {
+      const result = await dappcord.totalSupply()
+      expect(result).to.be.equal(ID)
+    })
+
+    it('Updates the contract balance', async () => {
+      const result = await ethers.provider.getBalance(dappcord.address)
+      expect(result).to.be.equal(AMOUNT)
+    })
+  })
+
+  describe("Withdrawing", () => {
+    const ID = 1
+    const AMOUNT = ethers.utils.parseUnits("10", 'ether')
+    let balanceBefore
+
+    beforeEach(async () => {
+      balanceBefore = await ethers.provider.getBalance(deployer.address)
+
+      let transaction = await dappcord.connect(user).mint(ID, { value: AMOUNT })
+      await transaction.wait()
+
+      transaction = await dappcord.connect(deployer).withdraw()
+      await transaction.wait()
+    })
+
+    it('Updates the owner balance', async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address)
+      expect(balanceAfter).to.be.greaterThan(balanceBefore)
+    })
+
+    it('Updates the contract balance', async () => {
+      const result = await ethers.provider.getBalance(dappcord.address)
+      expect(result).to.equal(0)
+    })
   })
 
 })
