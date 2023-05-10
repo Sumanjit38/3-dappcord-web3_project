2465fb9282820fac28dd8e911bf23cc5143480d4
Sumanjit38 <sumanjit38@gmail.com>
2023-05-10 12:19:26 +0530

    update nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,10 @@ const Navigation = ({ account, setAccount }) => {
 
   return (
     <nav>
+      <div className='nav__brand'>
+        <h1>Dappcord</h1>
+      </div>
+      <button type='button' className='nav__connect'>Connect</button>
 
     </nav>
   );
